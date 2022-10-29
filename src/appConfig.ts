f31f50b4e503644c0ca33280a85ea362c31d3ad4
arsenbelarus <arsenbelarus@gmail.com>
2022-10-29 17:18:32 +0200

    service config changes

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://api.github.com';
+export const BASE_URL = process.env.BASE_URL || 'https://api.github.com';
 
-export const TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+export const TOKEN = process.env.GITHUB_TOKEN;
